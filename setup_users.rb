821cc56c6beedf274cd132c1201503828dd0c8e2
jacksop <pjackson@mbs-projects.com>
2016-03-07 18:29:57 +0000

    Create organisations

--- setup_users.rb
+++ setup_users.rb
@@ -1,13 +1,26 @@
+aviva_properties = Property.where(name: 'A'..'B')
+ibm_properties = Property.where(name: 'I'..'J')
+
+# create organisations
+aviva_ou = Organisation.create! name: 'Aviva'
+ibm_ou = Organisation.create! name: 'IBM'
+
+aviva_properties.update_all(organisation_id: aviva_ou)
+ibm_properties.update_all(organisation_id: ibm_ou)
+
+
 # create BNPPRE users
 User.where("email like '%.bnppre@example.com'").each(&:destroy)
 
 %w(Brooke Bobby Bella Ben Blake).each do |u|
-  User.create!(username: u.downcase,
+  User
+    .create!(username: u.downcase,
               first_name: u,
               last_name: 'Babbage (BNPPRE)',
               email: "#{u}.bnppre@example.com",
               password: 'password',
               password_confirmation: 'password')
+    .organisations << [aviva_ou, ibm_ou]
 end
 
 User.find_by_username('brooke').update(admin_user: true)
@@ -18,12 +31,14 @@ User.find_by_username('bobby').update(admin_property: true)
 User.where("email like '%.aviva@example.com'").each(&:destroy)
 
 %w(Amelia Alfie Ava Archie).each do |u|
-  User.create!(username: u.downcase,
+  User
+    .create!(username: u.downcase,
               first_name: u,
               last_name: 'Adams (AVIVA)',
               email: "#{u}.aviva@example.com",
               password: 'password',
               password_confirmation: 'password')
+    .organisations = [aviva_ou]
 end
 
 User.find_by_username('amelia').update(admin_user: true)
@@ -34,14 +49,13 @@ User.find_by_username('alfie').update(admin_property: true)
 Property.unscoped.update_all deleted: false
 Property.unscoped.update_all archived: false
 
+
 # create access groups
 AccessGroup.destroy_all
 
 edit_template = AccessTemplate.find_by_name('Edit all')
 read_template = AccessTemplate.find_by_name('Read all')
 
-aviva_properties = Property.where(name: 'A'..'B')
-ibm_properties = Property.where(name: 'I'..'J')
 bnppre_select_properties = aviva_properties.first(5) + ibm_properties.first(2)
 aviva_select_properties = aviva_properties.first(2) + aviva_properties.last(5)
 
